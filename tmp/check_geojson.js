bbf0b5408dec7e374ef60ca387d2c95c12714cf4
lapolitiquecsip <lapolitiquecsimple@gmail.com>
2026-04-03 15:28:16 +0200

    chore: remove temp research files

--- tmp/check_geojson.js
+++
@@ -1,9 +0,0 @@
-
-async function checkGeoJSON() {
-    const url = "https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/circonscriptions-legislatives.geojson";
-    const resp = await fetch(url);
-    const data = await resp.json();
-    console.log("Total Features:", data.features.length);
-    console.log("Sample Properties:", data.features[0].properties);
-}
-checkGeoJSON();

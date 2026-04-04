090407694688f1e93ce17e27ae41813fb15597ad
lapolitiquecsip <lapolitiquecsimple@gmail.com>
2026-04-04 15:10:41 +0200

    style: replace broken Sénat image with verified Wikimedia photo

--- src/components/home/InstitutionsGrid.tsx
+++ src/components/home/InstitutionsGrid.tsx
@@ -32,7 +32,7 @@ const INSTITUTIONS: Institution[] = [
     id: "senat",
     name: "Sénat",
     shortName: "Sénat",
-    image: "https://photos.senat.fr/file/v/preview?id=299927&indexInCurrentSeach=0",
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a2/S%C3%A9nat_fran%C3%A7ais_Luxembourg.jpg",
     summary: "Le Palais du Luxembourg représente les collectivités territoriales.",
     color: "from-indigo-600",
     details: [

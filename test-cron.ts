d10969a7f546e1e8c35c2e3898d143f862f8536d
Hippolyte <hippo@example.com>
2026-06-27 17:15:12 +0200

    chore: remove deleted test script

--- test-cron.ts
+++
@@ -1,9 +0,0 @@
-import { GET } from './src/app/api/cron/update-laws/route';
-async function run() {
-  const req = new Request('http://localhost:3000/api/cron/update-laws', {
-    headers: { Authorization: `Bearer ${process.env.CRON_SECRET}` }
-  });
-  const res = await GET(req);
-  console.log(await res.json());
-}
-run();
